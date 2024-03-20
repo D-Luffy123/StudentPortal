4351f3d5f0a15ba4b7fab1af0d1194cbffadc02c
sairs <sairs@SAIR>
2024-03-20 16:44:56 +0545

    Add project files.

---
+++ Students/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Students.Models.Entities;
+namespace Students.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        
+
+        
+         
+
+    }
+}
---
+++ Students/Models/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace Students.Models.Entities
+{
+    public class Student
+    {
+        
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public string  email { get; set; }
+
+        public int PhoneNumber { get; set; }
+        public bool Subscribe { get; set; }
+    }
+}
